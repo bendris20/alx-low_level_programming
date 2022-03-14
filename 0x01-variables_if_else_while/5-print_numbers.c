8d119ec8085374417cbd878a0550d6f65ba3a59e
bendris20 <bendris20@jcu.edu>
2022-03-14 14:37:47 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-     int digit;
-	for (digit = 0 ; digit < 10; digit++)
-	{
-		printf("%digit", digit);
-	}
-	printf("\n");
+int digit;
+for (digit = 0 ; digit < 10; digit++)
+{
+printf("%digit", digit);
+}
+printf("\n");
 
-    return 0;
+return (0);
 }
