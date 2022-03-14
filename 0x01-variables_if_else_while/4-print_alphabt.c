60b0ff49c4a6809eeb6cbdc80170e627bf74a3fd
bendris20 <bendris20@jcu.edu>
2022-03-14 13:36:34 -0700

    Numbers

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,28 +2,14 @@
 
 int main(void)
 {
-    
-	for (char ch = 'a'; ch <= 'z'; ch++)
+    char ch = 'a';
+	while(ch <= 'z')
 	{
 		if (ch != 'e' && ch != 'q')
 		{
 			putchar(ch);
 		}
-	}
-	putchar('\n');
-
-    return 0;
-}
-
-int main(void)
-{
-    
-	for (char ch ='a'; ch <= 'z'; ch++)
-	{
-		if (ch == 'e' || ch == 'q')
-		    ch++;
-		putchar(ch);
-		
+		ch++;
 	}
 	putchar('\n');
 
