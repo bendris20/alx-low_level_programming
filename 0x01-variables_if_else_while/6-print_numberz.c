afa7ecf999b65e5168cfbd9b098106ceafaba949
bendris20 <bendris20@jcu.edu>
2022-03-14 15:24:07 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,12 +5,17 @@
  * Return: Always 0 (success/correct)
  */
 int main(void)
+
 {
 int i;
-for (int i = 48; i <= 57; i++)
+
+for (i = 48; i <= 57; i++)
 {
 putchar(i);
 }
+
 putchar('\n');
+
 return (0);
+
 }
