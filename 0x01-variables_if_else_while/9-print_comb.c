6865587ba5ffd4a502fe55bc9a94ba31b2285840
bendris20 <bendris20@jcu.edu>
2022-03-14 15:54:58 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
 int main(void)
 {
-    
-	for (int i = 0; i <= 9; i++)
-	{
-		putchar(i + '0');
-		if (i != 9) {
-		    putchar(',');
-		    putchar(' ');
-		}
-	}
-	putchar('\n');
+ 
+for (int i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+{
+putchar('\n');
 
-    return 0;
+return (0);
 }
